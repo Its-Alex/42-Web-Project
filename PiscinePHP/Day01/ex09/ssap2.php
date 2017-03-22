1c4752e610416f92cee3a1b62db721b0c45557c6
malexand <malexand@student.42.fr>
2017-03-22 18:36:43 +0100

    Send update ex09

--- PiscinePHP/Day01/ex09/ssap2.php
+++ PiscinePHP/Day01/ex09/ssap2.php
@@ -13,9 +13,32 @@ if ($argc > 1) {
             }
         }
     }
-    sort($array);
+    $numArray = array("begin");
+    $alphaArray = array("begin");
+    $specArray = array("begin");
+
     foreach ($array as $key => $value) {
-        echo $value . "\n";
+        if (preg_match('#^[a-zA-Z]#', $value))
+            array_push($alphaArray, $value);
+        else if (preg_match('#^[0-9]#', $value))
+            array_push($numArray, $value);
+        else
+            array_push($specArray, $value);
+    }
+    sort($alphaArray, SORT_STRING OR SORT_FLAG_CASE);
+    sort($numArray);
+    sort($specArray);
+    foreach ($alphaArray as $key => $value) {
+        if ($key != 0)
+            echo $value . "\n";
+    }
+    foreach ($numArray as $key => $value) {
+        if ($key != 0)
+            echo $value . "\n";
+    }
+    foreach ($specArray as $key => $value) {
+        if ($key != 0)
+            echo $value . "\n";
     }
 }
 ?>

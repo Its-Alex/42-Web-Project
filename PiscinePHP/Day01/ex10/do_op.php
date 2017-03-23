38ea2ecdc4a26ff31ffd3d45e8a056f613bf4dbc
malexand <malexand@student.42.fr>
2017-03-23 11:38:25 +0100

    Send do_op

--- PiscinePHP/Day01/ex10/do_op.php
+++ PiscinePHP/Day01/ex10/do_op.php
@@ -1,21 +1,21 @@
 #!/usr/bin/php
 <?php
-if ($argc > 1) {
+if ($argc === 4) {
      $argv[1] = trim($argv[1]);
      $argv[2] = trim($argv[2]);
      $argv[3] = trim($argv[3]);
 
-     if (strcmp($argv[2], "+") === 0)
+     if (strpos($argv[2], "+") !== false)
          $result = intval($argv[1]) + intval($argv[3]);
-     if (strcmp($argv[2], "-") === 0)
+     if (strpos($argv[2], "-") !== false)
          $result = intval($argv[1]) - intval($argv[3]);
-     if (strcmp($argv[2], "/") === 0)
+     if (strpos($argv[2], "/") !== false)
          $result = intval($argv[1]) / intval($argv[3]);
-     if (strcmp($argv[2], "*") === 0)
+     if (strpos($argv[2], "*") !== false)
          $result = intval($argv[1]) * intval($argv[3]);
-     if (strcmp($argv[2], "%") === 0)
+     if (strpos($argv[2], "%") !== false)
          $result = intval($argv[1]) % intval($argv[3]);
-    echo $result;
+    echo $result . "\n";
 }
 else {
     echo "Incorrect Parameters\n";

5d5edeaa3ca82553e90244a079913a59350ec1c1
malexand <malexand@student.42.fr>
2017-03-23 12:05:28 +0100

    Patch syntax do_op send do_op2

--- PiscinePHP/Day01/ex10/do_op.php
+++ PiscinePHP/Day01/ex10/do_op.php
@@ -1,20 +1,20 @@
 #!/usr/bin/php
 <?php
 if ($argc === 4) {
-     $argv[1] = trim($argv[1]);
-     $argv[2] = trim($argv[2]);
-     $argv[3] = trim($argv[3]);
+    $argv[1] = trim($argv[1]);
+    $argv[2] = trim($argv[2]);
+    $argv[3] = trim($argv[3]);
 
-     if (strpos($argv[2], "+") !== false)
-         $result = intval($argv[1]) + intval($argv[3]);
-     if (strpos($argv[2], "-") !== false)
-         $result = intval($argv[1]) - intval($argv[3]);
-     if (strpos($argv[2], "/") !== false)
-         $result = intval($argv[1]) / intval($argv[3]);
-     if (strpos($argv[2], "*") !== false)
-         $result = intval($argv[1]) * intval($argv[3]);
-     if (strpos($argv[2], "%") !== false)
-         $result = intval($argv[1]) % intval($argv[3]);
+    if (strpos($argv[2], "+") !== false)
+        $result = intval($argv[1]) + intval($argv[3]);
+    if (strpos($argv[2], "-") !== false)
+        $result = intval($argv[1]) - intval($argv[3]);
+    if (strpos($argv[2], "/") !== false)
+        $result = intval($argv[1]) / intval($argv[3]);
+    if (strpos($argv[2], "*") !== false)
+        $result = intval($argv[1]) * intval($argv[3]);
+    if (strpos($argv[2], "%") !== false)
+        $result = intval($argv[1]) % intval($argv[3]);
     echo $result . "\n";
 }
 else {
---
+++ PiscinePHP/Day01/ex11/do_op2.php
@@ -0,0 +1,57 @@
+#!/usr/bin/php
+<?php
+
+function calc($tab, $operator) {
+    $return = explode($operator, $tab);
+    if (preg_match("#[^ 0-9]#", ($return[0])) === 0 && preg_match("#[^ 0-9]#", ($return[1])) === 0)
+        return $return;
+    else
+        return false;
+}
+
+if ($argc === 2) {
+    $error = 0;
+    if (strpos($argv[1], "+") !== false) {
+        $array = calc($argv[1], "+");
+        if ($array !== false)
+            $result = intval($array[0]) + intval($array[1]);
+        else
+            $error = 1;
+    }
+    if (strpos($argv[1], "-") !== false) {
+        $array = calc($argv[1], "-");
+        if ($array !== false)
+            $result = intval($array[0]) - intval($array[1]);
+        else
+            $error = 1;
+    }
+    if (strpos($argv[1], "/") !== false) {
+        $array = calc($argv[1], "/");
+        if ($array !== false)
+            $result = intval($array[0]) / intval($array[1]);
+        else
+            $error = 1;
+    }
+    if (strpos($argv[1], "*") !== false) {
+        $array = calc($argv[1], "*");
+        if ($array !== false)
+            $result = intval($array[0]) * intval($array[1]);
+        else
+            $error = 1;
+    }
+    if (strpos($argv[1], "%") !== false) {
+        $array = calc($argv[1], "%");
+        if ($array !== false)
+            $result = intval($array[0]) % intval($array[1]);
+        else
+            $error = 1;
+    }
+    if ($result !== null && $error === 0)
+        echo $result . "\n";
+    else
+        echo "Syntax Error\n";
+}
+else {
+    echo "Incorrect Parameters\n";
+}
+?>

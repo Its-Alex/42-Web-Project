e3b6b7e8136bf8521b5349c7a3eff00d53b22577
Alexandre MARRE <xSkyZie@gmail.com>
2017-03-29 15:02:52 +0200

    Patch Day01/ex02

--- PiscinePHP/Day01/ex02/oddeven.php
+++ PiscinePHP/Day01/ex02/oddeven.php
@@ -1,21 +1,25 @@
 #!/usr/bin/php
 <?php
-$line;
-while (1) {
+$file_handle = @fopen("php://stdin","r");
+if ($file_handle) {
     echo "Entrer un nombre: ";
-    $line = trim(fgets(STDIN));
-    if ($line == NULL) {
-        echo "^D\n";
-        break;
+    while (($line = fgets($file_handle, 4096)) !== false) {
+        if (strlen($line) > 0);
+            $line = substr($line, 0, strlen($line) - 1);
+        if (preg_match("#[^0-9]#", $line) || strlen($line) === 0)
+            echo "'$line' n'est pas un chiffre\n";
+        else {
+            $nombre = intval($line);
+            if ($nombre % 2 === 0)
+                echo "Le chiffre $nombre est Pair\n";
+            else
+                echo "Le chiffre $nombre est Impair\n";
+        }
+        echo "Entrer un nombre: ";
     }
-    if (preg_match("#[^0-9]#", $line))
-        echo "'$line' n'est pas un chiffre\n";
-    else {
-        $nombre = intval($line);
-        if ($nombre % 2 === 0)
-            echo "Le chiffre $nombre est Pair\n";
-        else
-            echo "Le chiffre $nombre est Impair\n";
+    if (!feof($file_handle)) {
+        echo "Error: unexpected fgets() fail\n";
     }
+    fclose($file_handle);
 }
 ?>

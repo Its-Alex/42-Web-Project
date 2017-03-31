673b175bfbb5b459332ce48a94cf99a0221de9c2
Alexandre MARRE <xSkyZie@gmail.com>
2017-03-31 12:52:51 +0200

    Clean Day04/ex01

--- PiscinePHP/Day04/ex01/create.php
+++ PiscinePHP/Day04/ex01/create.php
@@ -1,28 +1,29 @@
 <?php
+	function error() {
+		echo "ERROR\n";
+		exit();
+	}
+
 	if (file_exists("./private/passwd"))
 		$array = unserialize(file_get_contents("./private/passwd"));
 	else
 		mkdir("./private");
-	if (isset($_POST['submit']) && strcmp($_POST['submit'], "OK") === 0) {
-		if (isset($_POST['login']) && isset($_POST['passwd']) && strlen($_POST['passwd']) > 0 && strlen($_POST['login']) > 0) {
-			if (isset($array)) {
-				foreach ($array as $key => $value) {
-					if (strcmp($value['login'], $_POST['login']) === 0) {
-						echo "ERROR\n";
-						exit();
-					}
-				}
-				array_push($array, array("login" => $_POST['login'], "passwd" => hash("whirlpool", $_POST['passwd'])));
-			}
-			else
-				$array = array(array("login" => $_POST['login'], "passwd" => hash("whirlpool", $_POST['passwd'])));
-			$array = serialize($array);
-			file_put_contents("./private/passwd", $array);
-			echo "OK\n";
+	if (!(isset($_POST['submit']) && strcmp($_POST['submit'], "OK") === 0))
+		error();
+	if (!(isset($_POST['login']) && isset($_POST['passwd']) && strlen($_POST['passwd']) > 0 && strlen($_POST['login']) > 0))
+		error();
+	if (isset($array))
+	{
+		foreach ($array as $key => $value)
+		{
+			if (strcmp($value['login'], $_POST['login']) === 0)
+				error();
 		}
-		else
-			echo "ERROR\n";
+		array_push($array, array("login" => $_POST['login'], "passwd" => hash("whirlpool", $_POST['passwd'])));
 	}
 	else
-		echo "ERROR\n";
+		$array = array(array("login" => $_POST['login'], "passwd" => hash("whirlpool", $_POST['passwd'])));
+	$array = serialize($array);
+	file_put_contents("./private/passwd", $array);
+	echo "OK\n";
 ?> 

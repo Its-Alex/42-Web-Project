21588f0053cf16184c3e03c1155fa0b3c1ce292f
Alexandre MARRE <xSkyZie@gmail.com>
2017-03-31 12:57:12 +0200

    Clean Day04/ex02

--- PiscinePHP/Day04/ex02/modif.php
+++ PiscinePHP/Day04/ex02/modif.php
@@ -1,43 +1,35 @@
 <?php
+	function error() {
+		echo "ERROR\n";
+		exit();
+	}
+
 	if (file_exists("./private/passwd"))
 		$array = unserialize(file_get_contents("./private/passwd"));
 	else
 		mkdir("./private");
-	if (isset($_POST['submit']) && strcmp($_POST['submit'], "OK") === 0)
+
+	if (!(isset($_POST['submit']) && strcmp($_POST['submit'], "OK") === 0))
+		error();
+	if (!(isset($_POST['login']) && isset($_POST['oldpw']) && strlen($_POST['oldpw']) > 0
+					&& isset($_POST['newpw']) && strlen($_POST['newpw']) > 0 && strlen($_POST['login']) > 0))
+		error();
+	if (isset($array))
 	{
-		if (isset($_POST['login']) && isset($_POST['oldpw']) && strlen($_POST['oldpw']) > 0
-				&& isset($_POST['newpw']) && strlen($_POST['newpw']) > 0 && strlen($_POST['login']) > 0)
+		foreach ($array as $key => $value)
 		{
-			if (isset($array))
+			if (strcmp($value['login'], $_POST['login']) === 0)
 			{
-				foreach ($array as $key => $value)
-				{
-					if (strcmp($value['login'], $_POST['login']) === 0)
-					{
-						if (strcmp($value['passwd'], hash("whirlpool", $_POST['oldpw'])) == 0)
-							$array[$key]['passwd'] = hash("whirlpool", $_POST['newpw']);
-						else
-						{
-							echo "ERROR\n";
-							exit();
-						}
-					}
-				}
-				$array = serialize($array);
-				file_put_contents("./private/passwd", $array);
-				echo "OK\n";
-			} else
-			{
-				echo "ERROR\n";
+				if (strcmp($value['passwd'], hash("whirlpool", $_POST['oldpw'])) == 0)
+					$array[$key]['passwd'] = hash("whirlpool", $_POST['newpw']);
+				else
+					error();
 			}
 		}
-		else
-		{
-			echo "ERROR\n";
-		}
+		$array = serialize($array);
+		file_put_contents("./private/passwd", $array);
+		echo "OK\n";
 	}
 	else
-	{
-		echo "ERROR\n";
-	}
+		error();
 ?>

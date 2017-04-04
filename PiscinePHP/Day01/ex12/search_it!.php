75de922463e7a6d89c9dfab8de2f667b1a943ad1
Alexandre MARRE <xSkyZie@gmail.com>
2017-04-04 11:06:32 +0200

    Patch some funcs

--- PiscinePHP/Day01/ex12/search_it!.php
+++ PiscinePHP/Day01/ex12/search_it!.php
@@ -1,9 +1,11 @@
 #!/usr/bin/php
 <?php
 
-function splitFirst($occu, $str) {
+function splitFirst($occu, $str)
+{
     $val = explode(":", $str);
-    foreach ($val as $key => $value) {
+    foreach ($val as $key => $value)
+    {
         if ($key === 0)
             $array = array($value);
         else if ($key === 1 && count($val) === 2)
@@ -20,8 +22,10 @@ function splitFirst($occu, $str) {
 
 if ($argc > 1)
 {
-    foreach ($argv as $key => $value) {
-        if ($key !== 0) {
+    foreach ($argv as $key => $value)
+    {
+        if ($key !== 0)
+        {
             $array = splitFirst(":", $value);
             if (count($array) > 1)
                 if (!strcmp($argv[1], $array[0]))

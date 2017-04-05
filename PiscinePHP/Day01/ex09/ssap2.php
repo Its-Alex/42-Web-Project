938592dbc43fa0b6167af4e6008b00c49235066f
Alexandre MARRE <xSkyZie@gmail.com>
2017-04-05 20:00:02 +0200

    Update ssap2

--- PiscinePHP/Day01/ex09/ssap2.php
+++ PiscinePHP/Day01/ex09/ssap2.php
@@ -1,44 +1,64 @@
 #!/usr/bin/php
 <?php
-if ($argc > 1) {
-    foreach ($argv as $key => $value)
-    {
-        if ($key !== 0)
-        {
-            if ($key === 1)
-                $array = explode(" ", $value);
-            else
-            {
-                foreach (explode(" ", $value) as $keys => $val)
-                    if (strlen($val) !== 0)
-                        array_push($array, $val);
-            }
+    function is_alpha($str){
+        $str = str_split(strtolower($str));
+        foreach ($str as $char) {
+            if (ord($char) < 97 || ord($char) > 122)
+                return (false);
         }
+        return (true);
     }
-    $numArray = array("");
-    $alphaArray = array("");
-    $specArray = array("");
-
-    foreach ($array as $key => $value)
+    function compare1($a, $b)
     {
-        if (preg_match('#^[a-zA-Z]#', $value))
-            array_push($alphaArray, $value);
-        else if (preg_match('#\d#', $value))
-            array_push($numArray, $value);
+        $a = strtolower($a);
+        $b = strtolower($b);
+        if (is_alpha($a))
+            $val_a = 1;
+        else if (is_numeric($a))
+            $val_a = 2;
         else
-            array_push($specArray, $value);
+            $val_a = 3;
+        if (is_alpha($b))
+            $val_b = 1;
+        else if (is_numeric($b))
+            $val_b = 2;
+        else
+            $val_b = 3;
+        if ($val_a != $val_b)
+            return ($val_a - $val_b);
+        return (strcmp($a, $b));
     }
-    sort($alphaArray, SORT_FLAG_CASE | SORT_STRING);
-    sort($numArray, SORT_STRING);
-    sort($specArray);
-    foreach ($alphaArray as $key => $value)
-        if (strlen($value) > 0)
-            echo $value . "\n";
-    foreach ($numArray as $key => $value)
-        if (strlen($value) > 0)
-            echo $value . "\n";
-    foreach ($specArray as $key => $value)
-        if (strlen($value) > 0)
-            echo $value . "\n";
-}
+    function compare($a, $b)
+    {
+        $i = 0;
+        while ($i < strlen($a) && $i < strlen($b))
+        {
+            if (compare1($a[$i], $b[$i]) > 0)
+                return (1);
+            else if (compare1($a[$i], $b[$i]) < 0)
+                return (-1);
+            $i++;
+        }
+        if ($i < strlen($a))
+            return (1);
+        else if ($i < strlen($b))
+            return (-1);
+        return (0);
+    }
+    function ft_split($str) {
+        $tab = explode(" ", $str);
+        $tab = array_values(array_filter($tab));
+        return ($tab);
+    }
+    $tab = $argv;
+    unset($tab[0]);
+    $tab = array_values($tab);
+    $tab2 = array();
+    foreach ($tab as $key => $value) {
+        $tab2 = array_merge($tab2, ft_split($value));
+    }
+    usort($tab2, "compare");
+    foreach ($tab2 as $value) {
+        echo $value."\n";
+    }
 ?>

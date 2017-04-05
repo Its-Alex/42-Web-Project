8a196676fadf892f0f258f496b76f01aeca49903
Alexandre MARRE <xSkyZie@gmail.com>
2017-04-05 14:31:04 +0200

    Patch ft_is_sort TRUE

--- PiscinePHP/Day01/ex08/ft_is_sort.php
+++ PiscinePHP/Day01/ex08/ft_is_sort.php
@@ -2,18 +2,27 @@
 <?php
 	function ft_is_sort($array)
 	{
+		$sort = 0;
 		$tabSort = $array;
 		sort($array);
 		foreach ($array as $key => $value)
+			echo $key." : ".$array[$key]." <===> ".$tabSort[$key]."\n";
 			if (strcmp($array[$key], $tabSort[$key]) !== 0)
-				return false;
-	  return true;
+			{
+				$sort++;
+				break;
+			}
+		$tabSort = array_reverse($tabSort);
+		foreach ($array as $key => $value)
+			echo $key." : ".$array[$key]." <===> ".$tabSort[$key]."\n";
+			if (strcmp($array[$key], $tabSort[$key]) !== 0)
+			{
+				$sort++;
+				break;
+			}
+		if ($sort == 1)
+			return true;
+		else
+			return false;
 	}
-
-	$tab = array(" ", "salut", "zZzZzZz", " ");
-if (ft_is_sort($tab))
-echo "Le tableau est trie\n";
-else
-echo "Le tableau n’est pas trie\n";
-
 ?>

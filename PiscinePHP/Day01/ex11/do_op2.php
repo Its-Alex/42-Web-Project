23fa47c08165431ae2fcb3a672ab0fb3ca21f997
Alexandre MARRE <xSkyZie@gmail.com>
2017-04-05 20:00:15 +0200

    Send Do_op2

--- PiscinePHP/Day01/ex11/do_op2.php
+++ PiscinePHP/Day01/ex11/do_op2.php
@@ -1,63 +1,39 @@
 #!/usr/bin/php
 <?php
-    function calc($tab, $operator)
-    {
-        $return = explode($operator, $tab);
-        foreach ($return as $key => $value)
-            if (strlen(value) < 1)
-                unset($return[$key]);
-        $return[0] = trim($return[0]);
-        $return[1] = trim($return[1]);
-        if (preg_match("#^[0-9]+$#", $return[0]) == 1 && preg_match("#^[0-9]+$#", $return[1]) == 1)
-            return $return;
-        else
-            return false;
+    function ft_split($str, $operator) {
+        $tab = explode($operator, $str);
+        $tab = array_values(array_filter($tab));
+        if (is_numeric(trim($tab[0])) && is_numeric(trim($tab[1])))
+            return ($tab);
+        echo "Syntax Error\n";
+        exit (-1);
     }
-
-    if ($argc === 2)
+    if ($argc == 2)
     {
-        $error = 0;
-        if (strpos($argv[1], "+") !== false && substr_count($argv[1], "+") == 1)
-        {
-            $array = calc($argv[1], "+");
-            if ($array !== false)
-                $result = intval($array[0]) + intval($array[1]);
-            else
-                $error = 1;
+        if (strpos($argv[1], "+") !== false) {
+            $str = ft_split($argv[1], "+");
+            $result = $str[0] + $str[1];
         }
-        if (strpos($argv[1], "-") !== false && substr_count($argv[1], "-") == 1)
-        {
-            $array = calc($argv[1], "-");
-            if ($array !== false)
-                $result = intval($array[0]) - intval($array[1]);
-            else
-                $error = 1;
+        elseif (strpos($argv[1], "-") !== false) {
+            $str = ft_split($argv[1], "-");
+            $result = $str[0] - $str[1];
         }
-        if (strpos($argv[1], "/") !== false && substr_count($argv[1], "/") == 1)
-        {
-            $array = calc($argv[1], "/");
-            if ($array !== false)
-                $result = intval($array[0]) / intval($array[1]);
-            else
-                $error = 1;
+        elseif (strpos($argv[1], "*") !== false) {
+            $str = ft_split($argv[1], "*");
+            $result = $str[0] * $str[1];
         }
-        if (strpos($argv[1], "*") !== false && substr_count($argv[1], "*") == 1)
-        {
-            $array = calc($argv[1], "*");
-            if ($array !== false)
-                $result = intval($array[0]) * intval($array[1]);
-            else
-                $error = 1;
+        elseif (strpos($argv[1], "/") !== false) {
+            $str = ft_split($argv[1], "/");
+            $result = $str[0] / $str[1];
         }
-        if (strpos($argv[1], "%") !== false && substr_count($argv[1], "%") == 1)
-        {
-            $array = calc($argv[1], "%");
-            if ($array !== false)
-                $result = intval($array[0]) % intval($array[1]);
-            else
-                $error = 1;
+        elseif (strpos($argv[1], "%") !== false) {
+            $str = ft_split($argv[1], "%");
+            $result = $str[0] % $str[1];
         }
-        if ($error == 0 && $result != null && isset($result))
+        else {
+            $error = true;
+        }
+        if ($error != true)
             echo $result."\n";
         else
             echo "Syntax Error\n";

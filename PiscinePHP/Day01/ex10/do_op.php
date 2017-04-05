9ed675a49086771c8b110e19f2d87700eeb98485
Alexandre MARRE <xSkyZie@gmail.com>
2017-04-05 13:31:37 +0200

    Patch Do_op1

--- PiscinePHP/Day01/ex10/do_op.php
+++ PiscinePHP/Day01/ex10/do_op.php
@@ -5,18 +5,20 @@ if ($argc === 4) {
     $argv[2] = trim($argv[2]);
     $argv[3] = trim($argv[3]);
 
-    if (strpos($argv[2], "+") !== false)
+    if (preg_match("#^(( +)?)\+(( +)?)$#", $argv[2]))
         $result = intval($argv[1]) + intval($argv[3]);
-    if (strpos($argv[2], "-") !== false)
+    else if (preg_match("#^(( +)?)-(( +)?)$#", $argv[2]))
         $result = intval($argv[1]) - intval($argv[3]);
-    if (strpos($argv[2], "/") !== false)
+    else if (preg_match("#^(( +)?)/(( +)?)$#", $argv[2]))
         $result = intval($argv[1]) / intval($argv[3]);
-    if (strpos($argv[2], "*") !== false)
+    else if (preg_match("#^(( +)?)\*(( +)?)$#", $argv[2]))
         $result = intval($argv[1]) * intval($argv[3]);
-    if (strpos($argv[2], "%") !== false)
+    else if (preg_match("#^(( +)?)%(( +)?)$#", $argv[2]))
         $result = intval($argv[1]) % intval($argv[3]);
     if (isset($result))
         echo $result."\n";
+    else
+        echo "Incorrect Parameters\n";
 }
 else {
     echo "Incorrect Parameters\n";

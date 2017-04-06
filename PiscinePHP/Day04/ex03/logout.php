ed584a0de5396335f9fbb27f6d2573949a3e2ede
Alexandre MARRE <xSkyZie@gmail.com>
2017-04-06 18:48:44 +0200

    Patch exo 4

--- PiscinePHP/Day04/ex03/logout.php
+++ PiscinePHP/Day04/ex03/logout.php
@@ -1,5 +1,5 @@
 <?php
 	session_start();
-	if (isset($_SESSION['loggued_on_user']))
-		unset($_SESSION['loggued_on_user']);
+	if ($_SESSION['loggued_on_user'] != "")
+		$_SESSION['loggued_on_user'] = "";
 ?>

493aea01e8800a746394acf351ec550259a375a1
Alexandre MARRE <xSkyZie@gmail.com>
2017-04-06 16:16:00 +0200

    Send Day04/ex03

--- PiscinePHP/Day04/ex03/auth.php
+++ PiscinePHP/Day04/ex03/auth.php
@@ -1,28 +1,19 @@
 <?php
 function auth($login, $passwd)
 {
-	$array = unserialize(file_get_contents("./private/passwd"));
-	if (isset($login) && isset($passwd) && strlen($passwd) > 0 && strlen($login) > 0)
-	{
-		if (isset($array))
-		{
-			foreach ($array as $key => $value)
-			{
-				if (strcmp($value['login'], $login) === 0)
-				{
-					if (strcmp($value['passwd'], hash("whirlpool", $passwd)) === 0)
-						echo "TRUE";
-					else
-						echo "FALSE";
-				}
-			}
-		}
-		else
-			echo "FALSE";
-	}
+
+	if (file_exists("../private/passwd"))
+		$array = unserialize(file_get_contents("../private/passwd"));
 	else
+		return false;
+	if (!isset($login) && !isset($passwd) && strlen($passwd) <= 0 && strlen($login) <= 0 && !isset($array))
+		return false;
+	foreach ($array as $key => $value)
 	{
-		echo "FALSE";
+		if ($value['login'] == $login)
+			if ($value['passwd'] == hash("whirlpool", $passwd))
+				return true;
 	}
+	return false;
 }
 ?>
--- PiscinePHP/Day04/ex03/login.php
+++ PiscinePHP/Day04/ex03/login.php
@@ -1,3 +1,15 @@
 <?php
+	session_start();
+	include('auth.php');
 
+	if (auth($_GET['login'], $_GET['passwd']) == true)
+	{
+		$_SESSION['loggued_on_user'] = $_GET['login'];
+		echo "OK\n";
+	}
+	else
+	{
+		$_SESSION['loggued_on_user'] = "";
+		echo "ERROR\n";
+	}
 ?>

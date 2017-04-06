a2b1b95aeb4de685498415738ea982e24c6500fc
Alexandre MARRE <xSkyZie@gmail.com>
2017-04-06 12:46:25 +0200

    Pacth Day01/ex06

--- PiscinePHP/Day03/ex06/members_only.php
+++ PiscinePHP/Day03/ex06/members_only.php
@@ -2,9 +2,9 @@
 <html><body>
 <?php
 	if (strcmp($_SERVER['PHP_AUTH_USER'], "zaz") === 0 && strcmp($_SERVER['PHP_AUTH_PW'], "jaimelespetitsponeys") === 0) {
-		$img = file_get_contents('/img/42.png');
+		$img = file_get_contents('../img/42.png');
 		$img_encode = base64_encode($img);
-		echo "Bonjour Zaz<br/>\nimg src='data:image/png;base64,".$img_encode."'>\n";
+		echo "Bonjour Zaz<br/>\n<img src='data:image/png;base64,".$img_encode."'>\n";
 	}
 ?>
 </body></html>

b55588186bc914b540ed3fad54e82cb4fb9a4fe7
Alexandre MARRE <xSkyZie@gmail.com>
2017-04-07 09:34:33 +0200

    Patch disallowed funcs in ex02

--- PiscinePHP/Day04/ex02/modif.php
+++ PiscinePHP/Day04/ex02/modif.php
@@ -1,37 +1,38 @@
 <?php
-	function error() {
-		echo "ERROR\n";
-		exit();
-	}
+	$count = 0;
+	$err = "";
 
 	if (file_exists("../private/passwd"))
 		$array = unserialize(file_get_contents("../private/passwd"));
-	else
+	else {
 		if (!file_exists("../private/"))
 			mkdir("../private");
+		$array = array();
+	}
 
-	if (!(isset($_POST['submit']) && strcmp($_POST['submit'], "OK") == 0))
-		error();
-	if (!(isset($_POST['login']) && isset($_POST['oldpw']) && strlen($_POST['oldpw']) > 0
-					&& isset($_POST['newpw']) && strlen($_POST['newpw']) > 0 && strlen($_POST['login']) > 0))
-		error();
-	if (isset($array))
+	if ($_POST['submit'] != "OK" || $_POST['login'] == "" || $_POST['oldpw'] == "" || $_POST['newpw'] == "")
+		$err = "ERROR\n";
+	if ($err != "ERROR\n")
 	{
 		foreach ($array as $key => $value)
 		{
 			if ($value['login'] == $_POST['login'])
 			{
-				if ($value['passwd'] == hash("whirlpool", $_POST['oldpw'])) {
+				if ($value['passwd'] == hash("whirlpool", $_POST['oldpw']))
 					$array[$key]['passwd'] = hash("whirlpool", $_POST['newpw']);
-				}
 				else
-					error();
+					$err = "ERROR\n";
+				$count = 1;
 			}
 		}
-		$array = serialize($array);
-		file_put_contents("../private/passwd", $array);
-		echo "OK\n";
+		if ($err != "ERROR\n" && $count == 1)
+		{
+			$array = serialize($array);
+			file_put_contents("../private/passwd", $array);
+			$err = "OK\n";
+		}
+		else
+			$err = "ERROR\n";
 	}
-	else
-		error();
+	echo $err;
 ?>

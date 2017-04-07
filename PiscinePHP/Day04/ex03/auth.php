976aa931166c6751700cf2fd1e8f9310903f4f61
Alexandre MARRE <xSkyZie@gmail.com>
2017-04-07 09:37:56 +0200

    Patch ex03

--- PiscinePHP/Day04/ex03/auth.php
+++ PiscinePHP/Day04/ex03/auth.php
@@ -1,12 +1,11 @@
 <?php
 function auth($login, $passwd)
 {
-
 	if (file_exists("../private/passwd"))
 		$array = unserialize(file_get_contents("../private/passwd"));
 	else
 		return false;
-	if (!isset($login) && !isset($passwd) && strlen($passwd) <= 0 && strlen($login) <= 0 && !isset($array))
+	if ($login == "" || $passwd == "")
 		return false;
 	foreach ($array as $key => $value)
 	{
--- PiscinePHP/Day04/ex03/login.php
+++ PiscinePHP/Day04/ex03/login.php
@@ -2,6 +2,7 @@
 	session_start();
 	include('auth.php');
 
+
 	if (auth($_GET['login'], $_GET['passwd']) == true)
 	{
 		$_SESSION['loggued_on_user'] = $_GET['login'];

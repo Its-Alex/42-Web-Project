b13970e317f38bd223a2b0983623721e2d85cb48
Alexandre MARRE <xSkyZie@gmail.com>
2017-04-09 20:12:24 +0200

    Update index

--- PiscinePHP/Rush00/index.php
+++ PiscinePHP/Rush00/index.php
@@ -8,8 +8,23 @@
     ?>
   <body>
     <?php include("./head_bar.php"); ?>
-    <div>
-      
+
+    <h1 class="super-titre">Bienvenue sur e-Shop !</h1>
+    <h1 class="super-titre">Le site de e-commerce en ligne le plus performant !</h1>  
+    <img src="https://blog.paymill.com/wp-content/uploads/2016/06/How-to-Keep-Your-E-Commerce-System-Funtioning-at-Optimum-Capacity.png" height=<8></8>00px>
+    <div class= "accueil">
+      <div>
+        <h2>Dernière nouveautées :</h2>
+        <p>Alex a ajouté Cookies (x9)</p>
+        <p>Alex a ajouté Ipod</p>
+        <p>Alex a ajouté Camping car</p>
+      </div>
+      <div>
+        <h2>Partenaire:</h2>
+        <p>Vente du diable</p>
+        <p>Vente privé</p>
+        <p>Leboncoin</p>
+      </div>
     </div>
   </body>
 </html>

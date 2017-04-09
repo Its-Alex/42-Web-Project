14ad74b6b70572b2367e3e19cde10d716de77a7b
Alexandre MARRE <xSkyZie@gmail.com>
2017-04-09 19:11:23 +0200

    Remove useless file

--- PiscinePHP/Rush00/controllers/footer.php
+++
@@ -1,5 +0,0 @@
-    <script type="text/javascript" src="./public/js/xhr.js"></script>
-    <script type="text/javascript" src="./public/js/movie.js"></script>
-    <script type="text/javascript" src="./public/js/exec.js"></script>
-  </body>
-</html>

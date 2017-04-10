550f7eaf6a7e7da98e3c6ccb2694ab56db7535d2
Alexandre MARRE <xSkyZie@gmail.com>
2017-04-10 16:50:59 +0200

    Patch

--- PiscinePHP/Day05/ex08/ex08.sql
+++ PiscinePHP/Day05/ex08/ex08.sql
@@ -1,1 +1,1 @@
-SELECT nom, prenom, YEAR(date_naissance) AS 'date de naissance' FROM fiche_personne WHERE YEAR(date_naissance)=1989 ORDER BY nom ASC;
+SELECT nom, prenom, YEAR(date_naissance) AS 'date de naissance' FROM fiche_personne WHERE YEAR(date_naissance) = 1989 ORDER BY nom ASC;

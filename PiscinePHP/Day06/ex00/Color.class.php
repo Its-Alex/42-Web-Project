7a8bf5085a66550d7d11afa635ec867812cccea2
Alexandre MARRE <xSkyZie@gmail.com>
2017-04-11 16:17:29 +0200

    Change Color

--- PiscinePHP/Day06/ex00/Color.class.php
+++ PiscinePHP/Day06/ex00/Color.class.php
@@ -5,7 +5,7 @@ Class Color {
     public $green;
     public $blue;
 
-    static $verbose = False;
+    public static $verbose = False;
 
     function __construct(array $kwargs) {
         if (array_key_exists('rgb', $kwargs)) {
@@ -13,46 +13,55 @@ Class Color {
             $this->green = (intval($kwargs['rgb'] & 0x00FF00) >> 8);
             $this->blue = (intval($kwargs['rgb'] & 0x0000FF));
         }
-        else {            
-            if (array_key_exists('red', $kwargs)) {
+        else {
+            if (array_key_exists("red", $kwargs))
                 $this->red = intval($kwargs['red']);
-            }
-            if (array_key_exists('green', $kwargs)) {
+            else
+                $this->red = 0;
+
+            if (array_key_exists("green", $kwargs))
                 $this->green = intval($kwargs['green']);
-            }
-            if (array_key_exists('blue', $kwargs)) {
+            else
+                $this->green = 0;
+
+            if (array_key_exists("blue", $kwargs))
                 $this->blue = intval($kwargs['blue']);
-            }
+            else
+                $this->blue = 0;
         }
         if (self::$verbose == True)
             print ("Color( red: "."$this->red".", green:   "."$this->green".", blue:   "."$this->blue"." ) constructed." . PHP_EOL);
     }
 
-    public function add(Color $addColor) {
-        $newColor = new Color(array($this->red, $this->green, $this->blue));
-        $newColor->red += $addColor->red;
-        $newColor->green += $addColor->green;
-        $newColor->blue += $addColor->blue;
-        return $newColor;
+    function __destruct() {
+        if (self::$verbose == True)
+            print ("Color( red: "."$this->red".", green:   "."$this->green".", blue:   "."$this->blue"." ) destructed." . PHP_EOL);
+    }
+
+    public function add(Color $rhs) {
+        return new Color( array("red" => $this->red + $rhs->red,
+                                 "green" => $this->green + $rhs->green,
+                                 "blue" => $this->blue + $rhs->blue));
+    }
+
+    public function sub(Color $rhs) {
+        return new Color( array("red" => $this->red - $rhs->red,
+                                 "green" => $this->green - $rhs->green,
+                                 "blue" => $this->blue - $rhs->blue));
+    }
+
+    public function mult($f) {
+        return new Color( array("red" => $this->red * $f,
+                                 "green" => $this->green * $f,
+                                 "blue" => $this->blue * $f));
     }
 
     public function __toString() {
-        if (self::$verbose == True)
-            return "Color( red: "."$this->red".", green:   "."$this->green".", blue:   "."$this->blue"." ) constructed.";
-        else
-            return "";
+        return "Color( red: "."$this->red".", green:   "."$this->green".", blue:   "."$this->blue"." )";
     }
 
     public static function doc() {
         return file_get_contents('Color.doc.txt').PHP_EOL;
     }
 }
-
-$red     = new Color( array( 'red' => 0xff, 'green' => 0   , 'blue' => 0    ) );
-$green   = new Color( array( 'rgb' => 255 << 8 ) );
-$blue    = new Color( array( 'red' => 0   , 'green' => 0   , 'blue' => 0xff ) );
-print( $red     . PHP_EOL );
-print( $green   . PHP_EOL );
-print( $blue    . PHP_EOL );
-
 ?>

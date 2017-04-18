25f18e67d09a244f66fcee0808100f3bc25a5215
Alex <xSkyZie@gmail.com>
2017-04-18 14:00:45 +0200

    Change for somes tests

--- camagru/public/js/exec.js
+++ camagru/public/js/exec.js
@@ -1,5 +1,5 @@
-function auth() {
-	var input = document.getElementsByClassName('connect');
+function signup() {
+	var input = document.getElementsByClassName('signup');
 	var str = "";
 
 	for (var i = 0; i < input.length - 1; i++) {
@@ -18,15 +18,22 @@ function auth() {
   });
 }
 
-window.onkeyup = (e) => {
-  if (e.keyCode === 32) {
-	  request(`POST`, `controllers/signin.php`, `mail=xkyZie@gmail.com&password=Apwn789123`, (res) => {
-    try {
-        console.log(JSON.parse(res));
-    } catch(e) {
-        console.error(e);
-        console.log(res)
-    }
-	  });
+function signin() {
+  var input = document.getElementsByClassName('signin');
+  var str = "";
+
+  for (var i = 0; i < input.length - 1; i++) {
+    if (i != input.length - 2)
+      str += input[i].name + '=' + input[i].value + '&';
+    else if (input[i].name)
+      str += input[i].name + '=' + input[i].value;
   }
+  request(`POST`, `controllers/signin.php`, str, (res) => {
+    try {
+      console.log(JSON.parse(res));
+    } catch(e) {
+      console.error(e);
+      console.log(res)
+    }
+  });
 }

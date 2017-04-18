51e8c31920596524284c4953e9990d3477fb7572
Alex <xSkyZie@gmail.com>
2017-04-18 14:00:33 +0200

    Begin views

--- camagru/views/body.php
+++ camagru/views/body.php
@@ -1,7 +1,14 @@
-    <div>
-      <input class="connect" type="text" name="name" placeholder="name">
-      <input class="connect" type="password" name="passwd" placeholder="Mot de passe">
-      <input class="connect" type="password" name="confirmPasswd" placeholder="Confirmer mot de passe">
-      <input class="connect" type="text" name="mail" placeholder="Mail">
-      <input class="connect" type="button" name="submit" value="Confirmer" onclick="auth()">
-    </div>
+		<div class="form-signin">
+			<input class="signin" type="text" name="name" placeholder="name">
+			<input class="signin" type="password" name="passwd" placeholder="Mot de passe">
+			<input class="signin" type="password" name="confirmPasswd" placeholder="Confirmer mot de passe">
+			<input class="signin" type="text" name="mail" placeholder="Mail">
+			<input class="signin" type="button" name="submit" value="Confirmer" onclick="signin()">
+		</div>
+		<div class="form-signup">
+			<input class="signup" type="text" name="name" placeholder="name">
+			<input class="signup" type="password" name="passwd" placeholder="Mot de passe">
+			<input class="signup" type="password" name="confirmPasswd" placeholder="Confirmer mot de passe">
+			<input class="signup" type="text" name="mail" placeholder="Mail">
+			<input class="signup" type="button" name="submit" value="Confirmer" onclick="signup()">
+		</div>
--- camagru/views/header.php
+++ camagru/views/header.php
@@ -6,5 +6,16 @@
     <meta name="viewport" content="width=device-width, user-scalable=no">
     <link rel="stylesheet" type="text/css" href="public/css/master.css">
     <link href="https://fonts.googleapis.com/css?family=Lato:300" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
   </head>
   <body>
+		<ul class="header">
+			<a onclick=""><li>Accueil</li></a>
+			<a onclick=""><li>Recherche</li></a>
+			<a onclick=""><li></li></a>
+			<a onclick=""><li>Connexion</li></a>
+			<a onclick="signin();"><li>Inscription</li></a>
+			<a onclick=""><li><i class="tiny material-icons">person_pin</i></li></a>
+			<a onclick=""><li><i class="tiny material-icons">supervisor_account</i></li></a>
+			<a onclick=""><li><i class="tiny material-icons">power_settings_new</i></li></a>
+		</ul>

3ccbf675e2a4dbb24eb594f152c3541f1837a686
Alex <xSkyZie@gmail.com>
2017-04-29 18:41:32 +0200

    Finish signup signin with css

--- camagru/public/js/ajax.js
+++ camagru/public/js/ajax.js
@@ -1,19 +1,46 @@
 HeadBar();
 
-function showSignUpForm() {
-  var docs = document.querySelectorAll('.hide');
-  docs.forEach((elem, index) => {
-    elem.style.visibility = 'hidden';
-    elem.hidden = true;
-  });
-  document.querySelector('.signup').style.visibility = 'visible';
-  document.querySelector('.signup').hidden = false;
+function deleteAllElem() {
+  var body = document.querySelectorAll('.hide');
+  body.forEach((elem) => {
+    while (elem.firstChild) elem.removeChild(elem.firstChild);
+  }, this);
 }
 
-function showSignInForm() {
-  var docs = document.querySelectorAll('.hide');
-  docs.forEach((elem, index) => {
-    elem.style.visibility = 'hidden';
-  });
-  document.querySelector('.signin').style.visibility = 'visible';
+function createInput(clas, type, name, placeholder) {
+  var input = document.createElement('input');
+  input.className = clas;
+  input.type = type;
+  input.name = name;
+  input.placeholder = placeholder;
+  return (input);
 }
+
+function createButton(clas, name, value, onclick) {
+  var input = document.createElement('input');
+  input.className = clas;
+  input.type = 'button';
+  input.name = name;
+  input.value = value;
+  input.onclick = onclick;
+  return (input);
+}
+
+function SignInForm() {
+  deleteAllElem();
+  var div = document.querySelector('.signin');
+  div.appendChild(createInput('signin', 'email', 'mail', 'Mail'));
+  div.appendChild(createInput('signin', 'password', 'passwd', 'Mot de passe'));
+  div.appendChild(createButton('signin', 'submit', 'Connexion', () => { signin(); }));
+}
+
+function SignUpForm() {
+  deleteAllElem();
+  var div = document.querySelector('.signup');
+  div.appendChild(createInput('signup', 'name', 'name', 'Nom'));
+  div.appendChild(createInput('signup', 'email', 'mail', 'Mail'));
+  div.appendChild(createInput('signup', 'password', 'passwd', 'Mot de passe'));
+  div.appendChild(createInput('signup', 'password', 'confirmPasswd', 'Confirmation mot de passe'));
+  div.appendChild(createButton('signup', 'submit', 'Inscription', () => { signup(); }));
+}
+
--- camagru/public/js/header.js
+++ camagru/public/js/header.js
@@ -13,9 +13,9 @@ function HeadBar () {
 
   }, () => {
 
-    showSignUpForm();
+    SignUpForm();
   }, () => {
-    showSignInForm();
+    SignInForm();
   }, () => {
 
   }, () => {

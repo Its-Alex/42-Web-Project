f0bd5a545762cb6560f4a90e9e15a4cda0433256
Alex <xSkyZie@gmail.com>
2017-05-04 02:10:03 +0200

    Test

--- camagru/config/database.php
+++ camagru/config/database.php
@@ -1,6 +1,6 @@
 <?php 
-  $DB_DSN = 'mysql:host=localhost;dbname=camagru';
-  $DB_USER = 'root';
+  $DB_DSN = 'mysql:host=sql.itsalex.fr;dbname=camagru';
+  $DB_USER = 'camagru';
   $DB_PASSWORD = '123456';
   $DB_EXTRA_PARAMS = array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8");
 ?>
--- camagru/config/setup.php
+++ camagru/config/setup.php
@@ -1,7 +1,7 @@
 <?php
   $sql = file_get_contents("./database.sql");
 
-  $pdo = new PDO("mysql:host=localhost", "root", "123456", array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+  $pdo = new PDO('mysql:host=sql.itsalex.fr;dbname=camagru', "camagru", "123456", array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
   $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   $pdo->query($sql);
   $pdo = null;

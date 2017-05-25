3faa8279392d80e8ae041bd23aeaa611127a8ec4
Alex <xSkyZie@gmail.com>
2017-05-25 18:08:11 +0200

    Update

--- camagru/public/js/utils.js
+++ camagru/public/js/utils.js
@@ -157,150 +157,28 @@ function putButton () {
 
 function showFilter(mountingDiv) {
   var filter = document.createElement('div');
+  var filterOn = document.querySelector('.filterOn');
+  if (filterOn == null) {
+    filterOn = document.createElement('img');
+  }
+  filterOn.className = 'filterOn';
+  mountingDiv.appendChild(filterOn);
+
   filter.className = 'filterDiv';
-
-  var filterChap1 = document.createElement('img');
-  filterChap1.src = 'public/assets/filter/chap1.png';
-  filterChap1.className = 'filterChoose';
-  filterChap1.onclick = () => {
-    var filterOn  = document.querySelector('.filterOn');
-    if (filterOn == null) {
-      filterOn = document.createElement('img');
-    }
-    filterOn.src = 'public/assets/filter/chap1.png';
-    filterOn.className = 'filterOn';
-    mountingDiv.appendChild(filterOn);
-    putButton();
-  };
-  var filterChap2 = document.createElement('img');
-  filterChap2.src = 'public/assets/filter/chap2.png';
-  filterChap2.className = 'filterChoose';
-  filterChap2.onclick = () => {
-    var filterOn  = document.querySelector('.filterOn');
-    if (filterOn == null) {
-      filterOn = document.createElement('img');
-    }
-    filterOn.src = 'public/assets/filter/chap2.png';
-    filterOn.className = 'filterOn';
-    mountingDiv.appendChild(filterOn);
-    putButton();
-  };
-  var filterChap3 = document.createElement('img');
-  filterChap3.src = 'public/assets/filter/chap3.png';
-  filterChap3.className = 'filterChoose';
-  filterChap3.onclick = () => {
-    var filterOn  = document.querySelector('.filterOn');
-    if (filterOn == null) {
-      filterOn = document.createElement('img');
-    }
-    filterOn.src = 'public/assets/filter/chap3.png';
-    filterOn.className = 'filterOn';
-    mountingDiv.appendChild(filterOn);
-    putButton();
-  };
-  var filterChap4 = document.createElement('img');
-  filterChap4.src = 'public/assets/filter/chap4.png';
-  filterChap4.className = 'filterChoose';
-  filterChap4.onclick = () => {
-    var filterOn  = document.querySelector('.filterOn');
-    if (filterOn == null) {
-      filterOn = document.createElement('img');
-    }
-    filterOn.src = 'public/assets/filter/chap4.png';
-    filterOn.className = 'filterOn';
-    mountingDiv.appendChild(filterOn);
-    putButton();
-  };
-  var filterChap5 = document.createElement('img');
-  filterChap5.src = 'public/assets/filter/chap5.png';
-  filterChap5.className = 'filterChoose';
-  filterChap5.onclick = () => {
-    var filterOn  = document.querySelector('.filterOn');
-    if (filterOn == null) {
-      filterOn = document.createElement('img');
-    }
-    filterOn.src = 'public/assets/filter/chap5.png';
-    filterOn.className = 'filterOn';
-    mountingDiv.appendChild(filterOn);
-    putButton();
-  };
-  var filterChap6 = document.createElement('img');
-  filterChap6.src = 'public/assets/filter/chap6.png';
-  filterChap6.className = 'filterChoose';
-  filterChap6.onclick = () => {
-    var filterOn  = document.querySelector('.filterOn');
-    if (filterOn == null) {
-      filterOn = document.createElement('img');
-    }
-    filterOn.src = 'public/assets/filter/chap6.png';
-    filterOn.className = 'filterOn';
-    mountingDiv.appendChild(filterOn);
-    putButton();
-  };
-  var filterChap7 = document.createElement('img');
-  filterChap7.src = 'public/assets/filter/chap7.png';
-  filterChap7.className = 'filterChoose';
-  filterChap7.onclick = () => {
-    var filterOn  = document.querySelector('.filterOn');
-    if (filterOn == null) {
-      filterOn = document.createElement('img');
-    }
-    filterOn.src = 'public/assets/filter/chap7.png';
-    filterOn.className = 'filterOn';
-    mountingDiv.appendChild(filterOn);
-    putButton();
-  };
-  var filterChap8 = document.createElement('img');
-  filterChap8.src = 'public/assets/filter/chap8.png';
-  filterChap8.className = 'filterChoose';
-  filterChap8.onclick = () => {
-    var filterOn  = document.querySelector('.filterOn');
-    if (filterOn == null) {
-      filterOn = document.createElement('img');
-    }
-    filterOn.src = 'public/assets/filter/chap8.png';
-    filterOn.className = 'filterOn';
-    mountingDiv.appendChild(filterOn);
-    putButton();
-  };
-  var filterChap9 = document.createElement('img');
-  filterChap9.src = 'public/assets/filter/chap9.png';
-  filterChap9.className = 'filterChoose';
-  filterChap9.onclick = () => {
-    var filterOn  = document.querySelector('.filterOn');
-    if (filterOn == null) {
-      filterOn = document.createElement('img');
-    }
-    filterOn.src = 'public/assets/filter/chap9.png';
-    filterOn.className = 'filterOn';
-    mountingDiv.appendChild(filterOn);
-    putButton();
-  };
-  var filterChap10 = document.createElement('img');
-  filterChap10.src = 'public/assets/filter/chap10.png';
-  filterChap10.className = 'filterChoose';
-  filterChap10.onclick = () => {
-    var filterOn  = document.querySelector('.filterOn');
-    if (filterOn == null) {
-      filterOn = document.createElement('img');
-    }
-    filterOn.src = 'public/assets/filter/chap10.png';
-    filterOn.className = 'filterOn';
-    mountingDiv.appendChild(filterOn);
-    putButton();
-  };
-
-  filter.appendChild(filterChap1);
-  filter.appendChild(filterChap2);
-  filter.appendChild(filterChap3);
-  filter.appendChild(filterChap4);
-  filter.appendChild(filterChap5);
-  filter.appendChild(filterChap6);
-  filter.appendChild(filterChap7);
-  filter.appendChild(filterChap8);
-  filter.appendChild(filterChap9);
-  filter.appendChild(filterChap10);
+  for (var count = 1; count < 11; ++count) {
+    var filterImg = document.createElement('img');
+    filterImg.src = 'public/assets/filter/chap' + count + '.png';
+    filterImg.className = 'filterChoose';
+    filterImg.setAttribute('onclick', 'onClickFilter(' + count + ')');
+    filter.appendChild(filterImg);
+  }
   return filter;
+}
+
+function onClickFilter (count) {
+  var filterOn = document.querySelector('.filterOn');
+  filterOn.src = 'public/assets/filter/chap' + count + '.png';
+  putButton();
 }
 
 window.onkeyup = (e) => {

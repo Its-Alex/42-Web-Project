2b37a3a83b813f3275dab742c37d66a0a5ad4e15
Alex <xskyzie@gmail.com>
2017-05-30 22:35:11 +0200

    Update forgetPwd to work

--- camagru/controllers/forgetPwd.php
+++ camagru/controllers/forgetPwd.php
@@ -1,21 +1,48 @@
 <?php
 	require_once dirname(__DIR__)."/models/User.class.php";
 
-	$user = new User(null);
 
 	function ret($success, $err) {
 		echo json_encode(array('success' => $success, 'err' => $err));
 		exit();
 	}
 
-	if (empty($_POST['mail']) || filter_var($_POST['mail'], FILTER_VALIDATE_EMAIL) == false)
-		ret(false, "Mail incorrect");
-	$user->mail = strtolower($_POST['mail']);
-  if (($user = $user->getUserByMail()) == null) {
-    ret(false, "L'utilisateur n'existe pas");
+	switch ($_SERVER['REQUEST_METHOD']) {
+		case 'POST':
+			$user = new User(null);
+
+			if (empty($_POST['mail']) || filter_var($_POST['mail'], FILTER_VALIDATE_EMAIL) == false)
+				ret(false, "Mail incorrect");
+			$user->mail = strtolower($_POST['mail']);
+			if (($user = $user->getUserByMail()) == null) {
+				ret(false, "L'utilisateur n'existe pas");
+			}
+			if ($user->sendForgotPasswdMailById() == true)
+				ret(true, "");
+			else
+				ret(false, "Le mail n'as pas été envoyé");
+			break;
+		case 'PUT':
+				$params = Utils::parseArgs(file_get_contents("php://input"));
+
+				if (empty($params['id']) || !isset($params['id'])
+						|| empty($params['passwd']) || !isset($params['passwd'])
+						|| empty($params['confirmPwd']) || !isset($params['confirmPwd']))
+					ret(false, "Wrong data");
+				if ($params['passwd'] != $params['confirmPwd'])
+					ret(false, "Password does not match");
+
+				$user = new User(null);
+				$user->id = $params['id'];
+				$user = $user->getUserById();
+				if ($user == null)
+					ret(false, "User not found");
+				$user->passwd = $params['passwd'];
+				if ($user->updateUser() == 0)
+						ret(false, 'Aucune modification');
+				ret(true, '');
+			break;
+		default:
+			ret(false, "API ERROR");
 	}
-  if ($user->sendForgotPasswdMailById() == true)
-    ret(true, "");
-  else
-    ret(false, "Le mail n'as pas été envoyé");
 ?>
--- camagru/forgetPwd.php
+++ camagru/forgetPwd.php
@@ -3,6 +3,39 @@
 ?>
 <!DOCTYPE html>
 <html>
+  <style>
+    html, body {
+      margin: 0;
+      padding: 0;
+      background-color: #fff;
+      font-family: 'Lato', sans-serif;
+      font-size: 1em;
+      width: 100%;
+      height: 100%;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      flex-direction: column;
+    }
+
+    input {
+      outline: none;
+      margin: 4px;
+      padding: 4px;
+      border: 1px solid #333;
+      border-radius: 4px;
+    }
+
+    input[type=button]:active {
+      border: 1px solid #eee;
+    }
+
+    input[type=button] {
+      background-color: #eee;
+      cursor: pointer;
+      min-height: 25px;
+    }
+  </style>
   <head>
     <title>Camagru</title>
     <meta charset="utf-8">
@@ -10,26 +43,23 @@
     <link href="https://fonts.googleapis.com/css?family=Lato:300" rel="stylesheet">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
   </head>
-  <div class="body">
+  <body>
     <input class="forgetPwd" type="password" name="passwd">
     <input class="forgetPwd" type="password" name="confirmPwd">
     <input class="forgetPwd" type="submit" name="submit" value="Confirmer" onclick="updatePassword()">
-	</div>
-  <body>
     <script type="text/javascript" src="./public/js/utils.js"></script>
     <script type="text/javascript">
       function updatePassword() {
-        var req = "token=" + window.location.href.split('=')[1] + "&";
-        req += window.location.href.split('?')[1] + "&";
+        var req = "id=" + window.location.href.split('=')[1] + "&";
         req += getElemForm('.forgetPwd');
-        if (getElemForm('.forgetPwd').split("=")[1].split("&")[0] !== getElemForm('.forgetPwd').split("=")[2]) {
-          console.log('diff');
-        } else {
-          console.log(req);
-          request('PUT', 'controllers/user.php', req, (res) => {
-            console.log(res);
-          });
-        }
+        request('PUT', 'controllers/forgetPwd.php', req, (res) => {
+          console.log(res);
+          res = JSON.parse(res);
+
+          if (res.success === true) {
+            document.location.href = 'http://localhost:3000';
+          }
+        });
       }
     </script>
   </body>

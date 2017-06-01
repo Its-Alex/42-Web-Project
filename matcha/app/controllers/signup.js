d7217064872628acc4e5bd00480cf2f86cca3773
Alex <xskyzie@gmail.com>
2017-06-01 13:04:55 +0200

    Send for test at 42

--- matcha/app/controllers/signup.js
+++ matcha/app/controllers/signup.js
@@ -1,19 +1,19 @@
 const model = require('../models/signup.js');
 const validator = require('validator');
-const uuid = require('uuid/V4');
+const uuid = require('uuid');
 
 module.exports = (body) => {
+  console.log(uuid.v4());
   return new Promise((resolve, reject) => {
-    if (validator.isEmail(req.body.mail) === false) {
+    if (validator.isEmail(body.mail) === false) {
       return reject('Mail invalid');
     } else {
-      req.body.mail = req.body.mail.toLowerCase();
+      body.mail = body.mail.toLowerCase();
     }
-    if (req.body.password)
-    if (req.body.password !== req.body.password1) {
+    if (body.password !== body.validPwd) {
       return reject('Password does not match');
     }
-    req.body.id = uuid();
+    body.id = uuid();
     model.checkIfUserExist('alex').then((results) => {
       console.log(results);
       return resolve();

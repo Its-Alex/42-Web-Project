8f545a16e3677478d2da5cd65b9d537ef1942e44
Alex <xskyzie@gmail.com>
2017-06-14 23:19:21 +0200

    Update CSS progil

--- matcha/front/src/routes/auth/profil.js
+++ matcha/front/src/routes/auth/profil.js
@@ -74,36 +74,16 @@ class Login extends Component {
         <div className='divForm'>
           <input type='date' name='birthday' onChange={this.handleChange} onKeyPress={this.handleKeyPress} />
           <textarea type='text' name='bio' value={this.state.email} placeholder='Bio' onChange={this.handleChange} onKeyPress={this.handleKeyPress} />
-          Genre:
-          <div className='radioContainer'>
-            <label htmlFor='genreMen'>
-              <input type='radio' name='genre' id='genreMen' onClick={this.handleChange} />
-              <p>Men</p>
-            </label>
-            <label htmlFor='genreWomen'>
-              <input type='radio' name='genre' id='genreWomen' onClick={this.handleChange} />
-              <p>Women</p>
-            </label>
-            <label htmlFor='genreAll'>
-              <input type='radio' name='genre' id='genreAll' onClick={this.handleChange} />
-              <p>All</p>
-            </label>
-          </div>
-          Type:
-          <div className='radioContainer'>
-            <label htmlFor='typeMen'>
-              <input type='radio' name='type' id='typeMen' onClick={this.handleChange} />
-              <p>Men</p>
-            </label>
-            <label htmlFor='typeWomen'>
-              <input type='radio' name='type' id='typeWomen' onClick={this.handleChange} />
-              <p>Women</p>
-            </label>
-            <label htmlFor='typeAll'>
-              <input type='radio' name='type' id='typeAll' onClick={this.handleChange} />
-              <p>All</p>
-            </label>
-          </div>
+          <select className='genre' name='genre' >
+            <option value='M' selected>Men</option>
+            <option value='F'>Woman</option>
+            <option value='B'>All</option>
+          </select>
+          <select className='type' name='type' >
+            <option value='M' selected>Men</option>
+            <option value='F'>Woman</option>
+            <option value='B'>All</option>
+          </select>
           <input type='text' name='tags' value={this.state.email} placeholder='Related tags' onChange={this.handleChange} onKeyPress={this.handleKeyPress} />
           <input type='submit' value='Submit' onClick={this.handleKeyPress} />
         </div>

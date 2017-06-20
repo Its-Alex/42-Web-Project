a9012717520aa8ab73a3b06d8b0e384abaff9165
Alexandre MARRE <xSkyZie@gmail.com>
2017-06-20 14:56:24 +0200

    Patch errors

--- matcha/front/src/navbar.js
+++ matcha/front/src/navbar.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import './navbar.css'
-import avatar from './img/avatar.svg'
 
 class Navbar extends Component {
   render () {
     return (
-      <div id='navbar'>      
+      <div id='navbar'>
         <img className='pictureProfil' src='//:0' />
         <input className='searchInput' type='text' placeholder='Search' />
         <ul>

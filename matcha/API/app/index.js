2195558e4ff2112bd76bdfc32036c2d95105937f
Alex <xskyzie@gmail.com>
2017-07-12 19:17:11 +0200

    Clean index

--- matcha/API/app/index.js
+++ matcha/API/app/index.js
@@ -3,13 +3,15 @@ const bodyParser = require('body-parser')
 const express = require('express')
 const app = express()
 const db = require('./db.js')
-db.connect()
-require('./ws.js')
 const port = 3005
 
+db.connect()
+require('./ws.js')
+
 app.disable('x-powered-by')
+
 app.use(cors())
-app.use(bodyParser.json({limit: '8mb'}))
+app.use(bodyParser.json({limit: '5mb'}))
 
 // Global api route
 app.use('/', require('./routes/index.js'))

d614b42df2f82f67febdeed65e1e62c6a13adc6f
Alex <xskyzie@gmail.com>
2017-07-13 22:41:16 +0200

    Patch minor error on update body

--- matcha/API/app/controllers/profil/update.js
+++ matcha/API/app/controllers/profil/update.js
@@ -11,27 +11,27 @@ function error (res, data, err) {
 
 module.exports = (req, res) => {
   if (!bcrypt.compareSync(req.body.password, req.user.password)) {
-    res.status(400)
-    return res.json({
-      success: false,
-      error: 'Wrong password'
-    })
+    return error(res, 'Wrong password', 400)
   }
 
   console.log(req.body)
 
   var profil = {id: req.user.id}
 
-  if (req.body.birthday !== undefined && req.body.birthday.match(/^[0-9]{4}-[0-9]{2}-[0-9]{2}$/)) {
+  if (req.body.birthday !== undefined && req.body.birthday !== '' && req.body.birthday.match(/^[0-9]{4}-[0-9]{2}-[0-9]{2}$/)) {
     let birth = req.body.birthday.split('-')
     if (birth[0] > new Date().getFullYear()) {
       return error(res, 'Birthday invalid', 400)
     }
     profil.birthday = req.body.birthday
+  } else {
+    return error(res, 'Birthday invalid', 400)
   }
 
-  if (req.body.bio !== undefined && req.body.bio.length < 120) {
+  if (req.body.bio !== undefined && req.body.bio !== '' && req.body.bio.length < 120) {
     profil.bio = req.body.bio
+  } else {
+    return error(res, 'Bio invalid', 400)
   }
 
   if (req.body.genre !== undefined && req.body.genre.length === 1 && (req.body.genre === 'M' ||
@@ -62,6 +62,8 @@ module.exports = (req, res) => {
 
   if (req.body.location !== undefined && req.body.location.match(/[a-zA-Z0-9,]/)) {
     profil.location = req.body.location
+  } else {
+    return error(res, 'Invalid location', 400)
   }
 
   model.updateProfil(profil).then(() => {

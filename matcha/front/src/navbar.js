3eeda5f3e81f87529a88471b10f2f297cc7ed967
Alex <xskyzie@gmail.com>
2017-07-13 21:59:35 +0200

    Finish profil picture

--- matcha/front/src/navbar.js
+++ matcha/front/src/navbar.js
@@ -1,23 +1,36 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import avatar from './img/avatar.svg'
+import axios from 'axios'
 import './navbar.css'
 
 class Navbar extends Component {
-  render () {
-    let profilPic
+  constructor (props) {
+    super(props)
 
+    this.state = {
+      profilPic: <img className='pictureProfil' src={avatar} alt='Profil' />
+    }
+  }
+
+  componentWillMount () {
     if (global.localStorage.getItem('Token') !== undefined) {
       let url = `http://localhost:3005/picture/${global.localStorage.getItem('Token')}/0`
-      profilPic = <img className='pictureProfil' src={url} alt='Profil' />
-    } else {
-      profilPic = <img className='pictureProfil' src={avatar} alt='Profil' />
+      axios.get(url).then((res) => {        
+        this.setState({
+          profilPic: <img className='pictureProfil' src={url} alt='Profil' />
+        })
+      }).catch((err) => {
+        if (err) console.log(err)
+      })
     }
+  }
 
+  render () {
     return (
       <div id='navbar'>
         <div id='imgContainer'>
-          {profilPic}
+          {this.state.profilPic}
           <div id='imgButtonContainer'>
             <Link to='/notifications'><div id='notifications' className='imgButton' alt='Notifications' /></Link>
             <Link to='/settings'><div id='settings' className='imgButton' alt='Settings' /></Link>

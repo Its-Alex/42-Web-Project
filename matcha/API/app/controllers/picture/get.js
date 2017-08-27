e61a817e303584e258f0ba190add5c66a606bc94
Alex <xskyzie@gmail.com>
2017-08-27 19:30:06 +0200

    Can get user's picture from ID or Token

--- matcha/API/app/controllers/picture/get.js
+++ matcha/API/app/controllers/picture/get.js
@@ -1,5 +1,6 @@
 // const model = require('../models/picture.js')
 const fs = require('fs')
+const validate = require('uuid-validate')
 const path = require('path')
 const userModel = require('../../models/user.js')
 const dir = path.dirname(require.main.filename) + '/pictures/'
@@ -13,22 +14,36 @@ function error (res, data, err) {
 }
 
 module.exports = (req, res) => {
-  if (req.params.token === undefined ||
-  !req.params.token.match(/[a-zA-Z0-9]{128}/)) {
+  if (req.params.token === undefined) return error(res, 'Bad token', 200)
+  if (req.params.token.match(/[a-zA-Z0-9]{128}/)) {
+    userModel.getUserByToken(req.params.token).then((user) => {
+      if (!fs.existsSync(dir + user[0].id) ||
+      !fs.existsSync(dir + user[0].id + '/' + req.params.id + '.png')) {
+        res.set('Content-Type', 'image/svg+xml')
+        res.sendFile(path.dirname(require.main.filename) + '/avatar.svg')
+      } else {
+        res.set('Content-Type', 'image/png')
+        res.sendFile(dir + user[0].id + '/' + req.params.id + '.png')
+      }
+    }).catch((err) => {
+      if (err) console.log(err)
+      error(res, 'Internal server error', 500)
+    })
+  } else if (validate(req.params.token, 4)) {
+    userModel.getUserById(req.params.token).then((user) => {
+      if (!fs.existsSync(dir + user[0].id) ||
+      !fs.existsSync(dir + user[0].id + '/' + req.params.id + '.png')) {
+        res.set('Content-Type', 'image/svg+xml')
+        res.sendFile(path.dirname(require.main.filename) + '/avatar.svg')
+      } else {
+        res.set('Content-Type', 'image/png')
+        res.sendFile(dir + user[0].id + '/' + req.params.id + '.png')
+      }
+    }).catch((err) => {
+      if (err) console.log(err)
+      error(res, 'Internal server error', 500)
+    })
+  } else {
     return error(res, 'Bad token', 200)
   }
-
-  userModel.getUserByToken(req.params.token).then((user) => {
-    if (!fs.existsSync(dir + user[0].id) ||
-    !fs.existsSync(dir + user[0].id + '/' + req.params.id + '.png')) {
-      res.set('Content-Type', 'image/svg+xml')
-      res.sendFile(path.dirname(require.main.filename) + '/avatar.svg')
-    } else {
-      res.set('Content-Type', 'image/png')
-      res.sendFile(dir + user[0].id + '/' + req.params.id + '.png')
-    }
-  }).catch((err) => {
-    if (err) console.log(err)
-    error(res, 'Internal server error', 500)
-  })
 }

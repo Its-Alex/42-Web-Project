89fe2f0a89235babce9d9fcbcaa7d597a6697228
Alex <xskyzie@gmail.com>
2017-09-09 23:03:46 +0200

    Reconnect to DB when connection is lost

--- matcha/API/app/db.js
+++ matcha/API/app/db.js
@@ -3,28 +3,37 @@ const mysql = require('mysql')
 var error
 var db
 
+let connect = () => {
+  db = mysql.createConnection({
+    host: 'localhost',
+    user: 'matcha',
+    password: '123456',
+    database: 'matcha',
+    debug: false
+  })
+  db.connect((err) => {
+    if (err) {
+      console.error('Error connecting: ' + err.stack)
+      error = err.stack
+    }
+    if (db.threadId !== null) {
+      console.log('Connected as id ' + db.threadId)
+    } else {
+      console.log('Connection to database failed!')
+      process.exit(1)
+    }
+  })
+  db.on('error', err => {
+    console.log(err)
+    if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+      db = connect()
+    }
+  })
+  return db
+}
+
 module.exports = {
-  connect: () => {
-    db = mysql.createConnection({
-      host: 'localhost',
-      user: 'matcha',
-      password: '123456',
-      database: 'matcha',
-      debug: false
-    })
-    db.connect((err) => {
-      if (err) {
-        console.error('Error connecting: ' + err.stack)
-        error = err.stack
-      }
-      if (db.threadId !== null) {
-        console.log('Connected as id ' + db.threadId)
-      } else {
-        console.log('Connection to database failed!')
-        process.exit(1)
-      }
-    })
-  },
+  connect,
   get: () => {
     return new Promise((resolve, reject) => {
       if (error === undefined) {

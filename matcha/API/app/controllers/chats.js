857b14501411e14bf62c7d9ed60edf2cfdd82d9d
Alex <xskyzie@gmail.com>
2017-09-19 16:22:15 +0200

    Remove blocked user on chatlist

--- matcha/API/app/controllers/chats.js
+++ matcha/API/app/controllers/chats.js
@@ -1,4 +1,6 @@
 const model = require('../models/chat.js')
+const modelBlock = require('../models/block.js')
+const async = require('async')
 
 function error (res, data, err) {
   res.status(err)
@@ -9,11 +11,23 @@ function error (res, data, err) {
 }
 
 module.exports = (req, res) => {
-  model.getChatPeople(req.user.id).then(chat => {
-    if (chat.length === 0) return error(res, 'No user found', 200)
-    res.send({
-      success: true,
-      chat: chat
+  model.getChatPeople(req.user.id).then(chats => {
+    if (chats.length === 0) return error(res, 'No user found', 200)
+    async.filter(chats, (chat, cb) => {
+      modelBlock.checkForNotif(req.user.id, chat.id).then(result => {
+        if (result === false) return cb(null, !chat)
+        return cb(null, chat)
+      }).catch(err => cb(err, null))
+    }, (err, chats) => {
+      console.log(chats)
+      if (err) {
+        console.log(err)
+        return error(res, 'Internal server', 500)
+      }
+      res.send({
+        success: true,
+        chat: chats
+      })
     })
   }).catch(err => {
     console.log(err)
